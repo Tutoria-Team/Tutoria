148a3753083701c760d58a333a04f3d3a970a9b2
Rita Lei <leir@rpi.edu>
2025-09-14 17:59:54 -0400

    Fix port number
    Co-Authored-By: Nicole Wu <143554135+Nicc041@users.noreply.github.com>
    Co-Authored-By: Tutoria-Team <230203310+Tutoria-Team@users.noreply.github.com>

--- server/index.js
+++ server/index.js
@@ -109,5 +109,5 @@ app.post('/api/login', async (req, res) => {
 
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
